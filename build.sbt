30a355255966dd489848673cc47fd04b3c2cb931
azaliaaknazarova <azalika.aknazarova@icloud.com>
2025-05-18 17:15:08 +0500

    Домашнее задание 6

--- build.sbt
+++ build.sbt
@@ -5,12 +5,14 @@ lazy val homework_2 = project.in(file("homework_2"))
 lazy val homework_3 = project.in(file("homework_3"))
 lazy val homework_4 = project.in(file("homework_4"))
 lazy val homework_5 = project.in(file("homework_5"))
+lazy val homework_6 = project.in(file("homework_6"))
 
 lazy val root = (project in file("."))
   .aggregate(homework_2)
   .aggregate(homework_3)
   .aggregate(homework_4)
   .aggregate(homework_5)
+  .aggregate(homework_6)
   .settings(
     name := "SparkDeveloper"
   )
---
+++ homework_6/src/main/scala/Main.scala
@@ -0,0 +1,74 @@
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    // Инициация спарка
+    val spark = SparkSession.builder()
+      .appName("Hw - Countries")
+      .master("local[*]")
+      .config("spark.log.level", "WARN")
+      .getOrCreate()
+
+    // Чтение данных из файла
+    val jsonDataDf = spark.read
+      .option("multiline", "true")
+      .json(getClass.getResource("/json/countries.json").getPath)
+
+    jsonDataDf.show(false)
+
+    countriesWithManyBorders(jsonDataDf).show(false)
+    manySpokenLanguages(jsonDataDf).show(false)
+
+    def countriesWithManyBorders(df: DataFrame): DataFrame = {
+      val result = df
+        .filter(size(col("borders")) >= 5)
+        .withColumn("NumBorders", size(col("borders")))
+        .withColumn("BorderCountries", concat_ws(",", col("borders")))
+        .select(
+          col("name.common").as("Country"),
+          col("NumBorders"),
+          col("BorderCountries")
+        )
+        .orderBy("Country")
+
+      result.write.mode("overwrite").parquet("output/countries_with_many_borders")
+      result
+    }
+
+    def manySpokenLanguages(df: DataFrame): DataFrame = {
+      import spark.implicits._
+
+      // Получаем список всех полей в struct languages из схемы
+      val languageFields = df.schema("languages").dataType
+        .asInstanceOf[org.apache.spark.sql.types.StructType]
+        .fields.map(_.name)
+
+      // Создаем массив из всех языков (полей languages)
+      val languagesArrayCol = array(languageFields.map(f => col(s"languages.$f")): _*)
+
+      // explode массива языков
+      val exploded = df
+        .select(
+          col("name.common").as("Country"),
+          explode(languagesArrayCol).as("Language")
+        )
+        .filter($"Language".isNotNull && length(trim($"Language")) > 0)
+
+      // Группируем по языку и считаем количество стран
+      val result = exploded
+        .groupBy("Language")
+        .agg(
+          countDistinct("Country").as("NumCountries"),
+          collect_set("Country").as("Countries")
+        )
+        .orderBy(desc("NumCountries"))
+
+      // Сохраняем результат
+      result.write.mode("overwrite").parquet("output/language_ranking")
+      result
+    }
+
+    spark.stop()
+  }
+}
